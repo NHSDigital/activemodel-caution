176111b009c232884a08eff54cc8343984ad2294
Brian Shand <Brian.Shand@phe.gov.uk>
2020-03-20 11:08:48 +0000

    # tag version for Rails 5.2.4.2 (no 5.2.3 -> 5.2.4.2 changes)

--- lib/active_model/caution/version.rb
+++ lib/active_model/caution/version.rb
@@ -1,7 +1,7 @@
 module ActiveModel
   module Caution
-    RAILS_VERSION = '5.2.3'.freeze
-    GEM_REVISION  = '1'.freeze
+    RAILS_VERSION = '5.2.4.2'.freeze
+    GEM_REVISION  = '0'.freeze
 
     # Gem version:
     VERSION = [RAILS_VERSION, GEM_REVISION].join('.')
